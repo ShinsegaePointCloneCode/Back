b8b73a085a65fb09a284f9da6188c2ff1794a526
ASY <showasy@naver.com>
2023-08-18 19:05:15 +0900

    Feat 전환포인트, 전환제휴사 Entity 추가

---
+++ smilekarina/src/main/java/com/example/smilekarina/point/domain/convert/ConvertedPartner.java
@@ -0,0 +1,24 @@
+package com.example.smilekarina.point.domain.convert;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+// 전환 제휴사
+@Builder
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ConvertedPartner {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, name = "converted_partner_name")
+    private String convertedPartnerName;
+
+}
