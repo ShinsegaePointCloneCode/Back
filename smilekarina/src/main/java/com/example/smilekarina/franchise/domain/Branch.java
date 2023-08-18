9906f0315561aae18c5ad8d92f3ad1eb4a89610e
DESKTOP-RL5EITB\교육생62 <003julie@gmail.com>
2023-08-18 10:35:39 +0900

    이번엔 제대로 될라나 ..? commit 빡세네 ..

--- smilekarina/src/main/java/com/example/smilekarina/franchise/domain/Branch.java
+++ smilekarina/src/main/java/com/example/smilekarina/franchise/domain/Branch.java
@@ -1,4 +1,23 @@
 package com.example.smilekarina.franchise.domain;
 
+import jakarta.persistence.*;
+
+@Entity
+//지점
 public class Branch {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(nullable = false, length=100, name="branch_address")
+    private String branchAddress;
+    @Column(nullable = false, length = 45, name="branch_name")
+    private String branchName;
+    /*
+    @Column(nullable = false,name = "branch_latitude")
+    private Float branchLatitude;   // 위도
+    @Column(nullable = false,name = "branch_lontitude")
+    private Float branchLontitude;  // 경도
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Franchise franchise;
 }
