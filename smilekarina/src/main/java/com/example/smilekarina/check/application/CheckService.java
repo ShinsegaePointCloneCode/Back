5d3585a089cb588f8f972a529475210cfcbe5c7d
goeom77 <7eom14@gmail.com>
2023-08-29 16:43:13 +0900

    Feat 유저 로그인시 가져오는 데이터 수정, 출석체크 get로직 추가

--- smilekarina/src/main/java/com/example/smilekarina/check/application/CheckService.java
+++ smilekarina/src/main/java/com/example/smilekarina/check/application/CheckService.java
@@ -1,8 +1,13 @@
 package com.example.smilekarina.check.application;
 
 
-import java.time.LocalDateTime;
+import com.example.smilekarina.check.domain.CheckPoint;
+import com.example.smilekarina.check.dto.CheckDateDto;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface CheckService {
+    List<CheckDateDto> getCheck(String token);
 //    void createCheck(String token, LocalDateTime time);
 }
