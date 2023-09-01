d73a4901e4cce015d0386bbeae699220e46fe285
goeom77 <7eom14@gmail.com>
2023-09-01 11:32:15 +0900

    Feat matcher edit

--- smilekarina/src/main/java/com/example/smilekarina/config/security/SecurityConfiguration.java
+++ smilekarina/src/main/java/com/example/smilekarina/config/security/SecurityConfiguration.java
@@ -29,7 +29,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors(AbstractHttpConfigurer::disable)
+//                .cors(AbstractHttpConfigurer::disable)
                 .csrf(CsrfConfigurer::disable) // CSRF 보안을 비활성화. API 서버로 사용하기 때문에 일반적으로 비활성화
 //                .authorizeHttpRequests(
 //                        authorizeHttpRequests -> authorizeHttpRequests
@@ -59,8 +59,8 @@ public class SecurityConfiguration {
                         sessionManagement -> sessionManagement
                                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 ) // 세션을 생성하지 않음. JWT 인증이기 때문에 상태가 없는(stateless) 세션 정책을 사용합니다.
-                .exceptionHandling(exceptionHandling -> exceptionHandling
-                        .authenticationEntryPoint(tokenEntryPoint))
+//                .exceptionHandling(exceptionHandling -> exceptionHandling
+//                        .authenticationEntryPoint(tokenEntryPoint))
                 .authenticationProvider(authenticationProvider) // 커스터마이징한 인증 제공자를 설정
                 // JWT 인증 필터를 UsernamePasswordAuthenticationFilter 전에 추가합니다.
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
