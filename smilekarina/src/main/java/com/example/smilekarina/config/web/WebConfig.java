fe7616c819f692894cf66fd1059ba176965eb196
goeom77 <7eom14@gmail.com>
2023-09-01 09:23:12 +0900

    debug cors annotation 처리 확인중

--- smilekarina/src/main/java/com/example/smilekarina/config/web/WebConfig.java
+++ smilekarina/src/main/java/com/example/smilekarina/config/web/WebConfig.java
@@ -9,8 +9,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/*")
-                .allowedOrigins("")
-                .allowedMethods("GET", "POST", "PUT")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT","DELETE","OPTIONS")
+                .allowedHeaders("*")
                 .maxAge(3000);
     }
 }
