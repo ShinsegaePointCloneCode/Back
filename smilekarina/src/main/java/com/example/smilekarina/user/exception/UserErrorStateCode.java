21402e7477ef4a0068f2a5b4ea34cc52661a3aae
goeom77 <7eom14@gmail.com>
2023-09-06 15:24:36 +0900

    Feat 쿠폰 조회하기

--- smilekarina/src/main/java/com/example/smilekarina/user/exception/UserErrorStateCode.java
+++ smilekarina/src/main/java/com/example/smilekarina/user/exception/UserErrorStateCode.java
@@ -10,7 +10,7 @@ public enum UserErrorStateCode {
     SAMEPREPASSWORD(false,3011,"이전 비밀번호와 동일합니다."),
     NOPASSWORD(false,3012,"비밀번호가 일치하지 않습니다.");
 
-    private final boolean isSuccess;
+    private final boolean success;
     private final int code;
     private final String message;
 }
